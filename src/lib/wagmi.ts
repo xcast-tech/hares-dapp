ba1fa00d1ddbc10dbe545e40314092fd6b8f41f1
dev <dev@web3.org>
2024-12-17 00:08:26 +0800

    feat: wallet

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,18 +1,18 @@
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import { binanceWallet, metaMaskWallet, okxWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
-import { createClient, http } from 'viem';
-import { createConfig } from 'wagmi';
-import { mainChain } from '@/lib/constant';
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { metaMaskWallet, okxWallet, trustWallet, coinbaseWallet, rainbowWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
+import { createClient, http } from "viem";
+import { createConfig } from "wagmi";
+import { mainChain } from "@/lib/constant";
 
 const connectors = connectorsForWallets(
   [
     {
       groupName: "Recommended",
-      wallets: [metaMaskWallet],
+      wallets: [coinbaseWallet, rainbowWallet],
     },
     {
       groupName: "Popular",
-      wallets: [metaMaskWallet, trustWallet, okxWallet],
+      wallets: [metaMaskWallet, walletConnectWallet, trustWallet, okxWallet],
     },
   ],
   {
@@ -25,9 +25,9 @@ export const wagmiConfig = createConfig({
   connectors,
   chains: [mainChain],
   client({ chain }) {
-    return createClient({ chain, transport: http() })
+    return createClient({ chain, transport: http() });
   },
   ssr: true,
-})
+});
 
-export const publicClient = createClient({ chain: mainChain, transport: http() })
+export const publicClient = createClient({ chain: mainChain, transport: http() });
