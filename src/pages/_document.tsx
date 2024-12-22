b4cd90f25dec7562d1bbf116205d05cd7de75678
J <jie@communities.id>
2024-12-23 01:28:53 +0800

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react"
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
 };
@@ -13,6 +14,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Analytics/>
       </body>
     </Html>
   );
