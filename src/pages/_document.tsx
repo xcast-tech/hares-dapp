92ab4f234ed25f0d147c0c882c2b09aad59dbece
dev <dev@dev.dev>
2025-01-05 22:42:10 +0800

    feat: viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="https://6aaae0hsgx5fvfpg.public.blob.vercel-storage.com/haresai/common/logo-TnSx9q4tVNiiEa7d076NspHfRs3Cyb.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body className="antialiased">
         <Main />
