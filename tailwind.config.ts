b507965a47e368bb3f00bc5f9119c43dbed4d731
gene-ht <snx.gene@gmail.com>
2025-03-01 17:31:14 +0800

    upt: drawer right style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,10 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
