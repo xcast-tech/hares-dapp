dd6df681359e24237adb569e60c8a8ed9a76f702
J <jie@communities.id>
2025-03-19 00:43:04 +0800

    update contract

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -13,9 +13,9 @@ export const SCAN_API_KEY = process.env[`SCAN_API_KEY_${mainChain.id}`];
 
 export const tokenSymbol = mainChain.nativeCurrency.symbol;
 export const contractAddress = {
-  BondingCurve: "0x3E2c77C7591a57beEc4Fd759407D17d19C78385E" as Address,
-  BABTValidatorAddress: "0xB5Fb5724a204E83fE894C1b67A04B0ad6eD300F7" as Address,
-  HaresFactory: "0x69EfCb45b473A0Af765b52d0267534E01F2208FB" as Address,
+  BondingCurve: "0x17334F09aA1e09Ec2A4fEEF9533fBBC99ADa0fb8" as Address,
+  BABTValidatorAddress: "0x2E0Eb54F0A52D6F20b77Bac37b4bC68F6b98e2c6" as Address,
+  HaresFactory: "0x84fD1cfE0c65D46C356119629D4B5935891CbE84" as Address,
   WETH: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c" as Address,
 };
 
@@ -28,16 +28,11 @@ export const ABIs = {
 };
 
 export const EventTopic: Record<string, string> = {
-  HaresTokenCreated:
-    "0x4d70dd94558eb7f121037c7e1f34da1e1a7a11ea457ab927bd55e7810870541d",
-  HaresTokenTransfer:
-    "0x4094f1cf9c22c433db0b220c48bc94f47e2c93296f56a8b9fc33a69ddab53882",
-  HaresTokenBuy:
-    "0x03d6fa0abbf51ed965299837d77d70b68ac82c0bd8e2d104c056d3d135abc767",
-  HaresTokenSell:
-    "0x0356859bb57a8e38f4212c48a3dd4c5726c382ff19ad19b90a0fdd80a5e35bda",
-  HaresMarketGraduated:
-    "0xb1eedf734432e24ee0d06ba360e9f293c6135315787e97012415419e8fb95645",
+  HaresTokenCreated: "0x4d70dd94558eb7f121037c7e1f34da1e1a7a11ea457ab927bd55e7810870541d",
+  HaresTokenTransfer: "0x4094f1cf9c22c433db0b220c48bc94f47e2c93296f56a8b9fc33a69ddab53882",
+  HaresTokenBuy: "0x03d6fa0abbf51ed965299837d77d70b68ac82c0bd8e2d104c056d3d135abc767",
+  HaresTokenSell: "0x0356859bb57a8e38f4212c48a3dd4c5726c382ff19ad19b90a0fdd80a5e35bda",
+  HaresMarketGraduated: "0xb1eedf734432e24ee0d06ba360e9f293c6135315787e97012415419e8fb95645",
 };
 
 export const BondingCurveConfig = {
@@ -54,5 +49,4 @@ export const graduatedPoolConstant = "0.0619"; // 18.91 / 0.99
 
 export const primaryMarketSupply = parseEther((8e8).toString()); // 800,000,000
 
-export const loginSignText =
-  "Sign in to bab.fun. Discover the most authentic token launchpad, powered by Binance BAB Token. This action only verifies your wallet ownership and will not incur any gas fees.";
+export const loginSignText = "Sign in to bab.fun. Discover the most authentic token launchpad, powered by Binance BAB Token. This action only verifies your wallet ownership and will not incur any gas fees.";
